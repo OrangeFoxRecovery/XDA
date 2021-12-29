4fa8566eaad1be46143f32471132a3b9641c7806
Sushrut1101 <guptasushrut@gmail.com>
2021-12-30 01:10:08 +0530

    Define Some Vars
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- gen_xda_template.py
+++ gen_xda_template.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 
+# Variables
+FDEVICE = ""                # Device Codename
+DEVICE_COMMON_NAME = ""     # Device Common Name
+BUILD_DATE = ""             # Date of the Build
+XDA_INIT_DATA = ""          # Date when the XDA Thread was Created
+DOWNLOAD_LINK = ""          # Download Link of your Build
+FOX_BUILD_TYPE = ""         # Beta / Stable / Unofficial
+
+# Output File
 OUTFILE = "./output.txt"
