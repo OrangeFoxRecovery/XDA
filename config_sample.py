166313463085958f9f9814cc952093bd16443a9d
Sushrut1101 <guptasushrut@gmail.com>
2022-01-02 16:49:24 +0530

    Use config.py for the Variables
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

---
+++ config_sample.py
@@ -0,0 +1,20 @@
+# Variables
+FDEVICE = ""                # Device Codename
+DEVICE_COMMON_NAME = ""     # Device Common Name
+RELEASE_DATE = ""           # Date of Release of the Build
+XDA_INIT_DATA = ""          # Date when the XDA Thread was Created
+DOWNLOAD_LINK = ""          # Download Link of your Build
+FOX_BUILD_TYPE = ""         # Beta / Stable / Unofficial
+FOX_VERSION = ""            # The Full Version of the Release. (eg. "R11.1_1")
+
+FOX_SOURCES = "https://gitlab.com/OrangeFox"
+SOURCE_CHANGELOG = "https://wiki.orangefox.tech/changelog"
+NEWS_CHANNEL = ""           # Telegram Updates Channel
+SUPPORT_CHAT = ""           # Telegram Support Chat
+
+DEVICE_TREE = ""            # The Link of the Device Tree
+KERNEL = ""                 # The Link of the Kernel (Source or Prebuilt)
+
+MAINTAINER = ""             # Name of the Maintainer
+MAINTAINER_XDA_URL = ""     # XDA URL of the Maintainer
+DONATION_URL = ""           # Your Donation Link. Leave Blank if None.
--- main.py
+++ main.py
@@ -1,31 +1,11 @@
 #!/usr/bin/env python3
 
-# Variables
-FDEVICE = ""                # Device Codename
-DEVICE_COMMON_NAME = ""     # Device Common Name
-RELEASE_DATE = ""           # Date of Release of the Build
-XDA_INIT_DATA = ""          # Date when the XDA Thread was Created
-DOWNLOAD_LINK = ""          # Download Link of your Build
-FOX_BUILD_TYPE = ""         # Beta / Stable / Unofficial
-FOX_VERSION = ""            # The Full Version of the Release. (eg. "R11.1_1")
-
-FOX_SOURCES = "https://gitlab.com/OrangeFox"
-SOURCE_CHANGELOG = "https://wiki.orangefox.tech/changelog"
-NEWS_CHANNEL = ""           # Telegram Updates Channel
-SUPPORT_CHAT = ""           # Telegram Support Chat
-
-DEVICE_TREE = ""            # The Link of the Device Tree
-KERNEL = ""                 # The Link of the Kernel (Source or Prebuilt)
-
-MAINTAINER = ""             # Name of the Maintainer
-MAINTAINER_XDA_URL = ""     # XDA URL of the Maintainer
-DONATION_URL = ""           # Your Donation Link. Leave Blank if None.
+# Import Required Libraries
+import sys
+from config import *
 
 # Output File
 OUTFILE = "output.txt"
-
-# Import Required Libraries
-import sys
 
 # Open the Output File in Write Mode
 FILE = open(OUTFILE,w)
